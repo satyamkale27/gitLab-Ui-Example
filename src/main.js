f402ededf4f918da24112f674259daac12ce6c70
satyam <satyamkale271@gmail.com>
2024-08-25 19:06:04 +0530

    working

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import setConfigs from "@gitlab/ui/dist/config";
 
-createApp(App).mount('#app')
+setConfigs();
+
+new Vue({
+  render: (h) => h(App),
+}).$mount("#app");
